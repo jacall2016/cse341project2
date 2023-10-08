3725685233c36b6cbedc0e5b44b9c7d2cee58ca2
James Call <jcall2016@byui.edu>
2023-10-07 18:30:47 -0600

    updated authenticate

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,7 +1,9 @@
+//authenticate
 const isAuthenticated = (req, res, next) => {
     if (req.session.user === undefined){
         return res.status(401).json("You do not have access.");
     }
+
     next();
 };
 
